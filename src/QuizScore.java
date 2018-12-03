4e1fefc912d5620186e0b96423faa2a822970789
Ian Bryan <ianbryanz@gmail.com>
2018-12-03 14:40:05 -0800

    2 New Classes
    Added QuizScore which is a very simple class that has only one data item to represent an integer score for a quiz. QuizTracker contains a list of those scores.

--- src/QuizScore.java
+++ src/QuizScore.java
@@ -7,4 +7,27 @@
  */
 public class QuizScore {
 
+	/**
+	 * Class level data members.
+	 * */
+	private int score;
+	
+	/**
+	 * @param score
+	 */
+	public QuizScore(int score) {
+		this.setScore(score);
+	}
+	
+	/**
+	 * @return
+	 */
+	public int getScore() {
+		return this.score;
+	}
+	
+	/**
+	 * @param score
+	 */
+	public void setScore(int score) {	this.score = score;}
 }
---
+++ src/QuizTracker.java
@@ -0,0 +1,9 @@
+/**
+ * Class Description:
+ * @author Ian Bryan
+ * @version December 3rd, 2018.
+ * 
+ */
+public class QuizTracker {
+
+}
